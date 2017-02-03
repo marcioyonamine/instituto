3e41f446e7f772db43b42134f74abf1373fbf177
thiago <thiagonegro@gmail.com>
2017-02-03 17:07:37 -0200

    arquivos pra montar o relatorio de fase

--- inc/db.php
+++ inc/db.php
@@ -1,9 +1,9 @@
 ﻿<?php
 function bancoMysqli(){ 
 	$servidor = 'localhost';
-	$usuario = 'root';
-	$senha = 'lic54eca';
-	$banco = 'instituto';
+	$usuario = 'ialtaper_adm';
+	$senha = 'iap#2017';
+	$banco = 'ialtaper_aplicativo';
 	$con = mysqli_connect($servidor,$usuario,$senha,$banco); 
 	mysqli_set_charset($con,"utf8");
 	return $con;
