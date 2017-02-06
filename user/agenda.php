37a9c35fca1f49fdee1f15e30c7f3bedde4ade57
Marcio Yonamine <marcioyonamine@gmail.com>
2017-02-06 21:00:06 -0200

    segunda funcionando

--- user/agenda.php
+++ user/agenda.php
@@ -1,4 +1,10 @@
 <?php include '../inc/header.php';?>
+<?php 
+
+
+
+?>
+
 
 <div class="container">
 	<?php include '../inc/fixed-navbar-user.php'; ?>
@@ -7,6 +13,30 @@
 	<div class="jumbotron">
 		<?php echo "<h1>Agenda</h1>"; ?>
 	</div>
+
+<?php 
+      $events = eo_get_events(array(
+              'numberposts'=>5,
+              'event_start_after'=>'today',
+              'showpastevents'=>true,//Will be deprecated, but set it to true to play it safe.
+         ));
+
+       if($events):
+          echo '<ul>';
+          foreach ($events as $event):
+               //Check if all day, set format accordingly
+               $format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
+               printf(
+                  '<li><a href="%s"> %s </a> on %s </li>',
+                  get_permalink($event->ID),
+                  get_the_title($event->ID),
+                  eo_get_the_start( $format, $event->ID, $event->occurrence_id )
+               );
+          endforeach;
+          echo '</ul>';
+       endif;
+
+?>
 
 	
 		<?php include '../inc/footer.php'; ?>
