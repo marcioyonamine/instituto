0725bcfa394190b13749c20666c8f26c05f5027f
Marcio Yonamine <marcioyonamine@gmail.com>
2017-04-10 19:18:48 -0300

    UPDATE data Adv

--- man/advertencias.php
+++ man/advertencias.php
@@ -39,8 +39,16 @@ switch($diasemana){
 	}
 	break;
 	
-	case 3: // verifica se é quarta-feira
-
+	case 3: // verifica se é quarta-feira 
+		
+	/* 
+	inserir "Usuário não enviou relatório em tempo" na tabela iap_termos
+	pegar o ID e colocar na variável $id_adv
+	mudar o número de INT do campo advertencia da tabela iap_advertencia para 3
+	
+	*/
+	$id_adv = "";
+	
 	//se sim, carrega todos os objetivos que possuam desafios definidos
 	$sql = "SELECT * FROM iap_aceite, iap_objetivo 
 	WHERE iap_aceite.objetivo = iap_objetivo.id 
@@ -63,7 +71,28 @@ switch($diasemana){
 			//echo "A semana $sem do objetivo $obj_id NÃO possui relatório.<br />";
 			$insere_adv = "INSERT INTO iap_advertencia ('id', 'usuario', 'fase', 'objetivo', 'semana', 'advertencia', 'publicado') VALUES (NULL, '$user->ID', '$fase_atual', '$obj_id', '$sem', '1', '1')";
 			$exec_adv = mysqli_query($con, $insere_adv);
-			emailTreinador("advertencia",$user->user_firstname,$user->user_email); 
+			emailTreinador("advertencia",$user->user_firstname,$user->user_email);
+			$sql_verifica_adv = "SELECT id FROM iap_advertencia WHERE usuario = '".$x['usuario']."' AND publicado = '1' AND advertendia = '$id_adv' ";
+			$query_verifica_adv = mysqli_query($con,$sql_verifica_adv);
+			$num_verifica_adv = mysqli_num_rows($query_verifica_adv);
+			switch($num_verifica_adv){
+			
+			case 0:
+			case 1:
+			case 2:
+				// muda a data próxima segunda
+				$prox_seg = nextMonday($obj['data_inicio']);				
+				$sql_atualiza_data = "UPDATE iap_objetivo SET data_inicio = '$prox_seg' WHERE id = '$obj_id'";
+				$query_atualiza_data = mysqli_query($con,$sql_atualiza_data);
+				if($query_atualiza_data){
+					gravarLog($sql_atualiza_data,$x['usuario']);
+				}else{
+					gravarLog("Erro ao fazer UPDATE na data nextMonday",$x['usuario']);	
+				}
+			break;		
+			
+			}
+			 
 		}else{
 			echo "A semana $sem do objetivo $obj_id possui relatório.<br />";
 		}	
